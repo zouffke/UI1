959b31fee717878769e051c42aaf23ed96614649
Luca.Dand <Luca.dandois@student.kdg.be>
2022-12-15 12:24:26 +0100

    M3.2_V3 fiche span in JavaScript aangemaakt

--- script/dyn_result.js
+++ script/dyn_result.js
@@ -5,8 +5,24 @@ function INIT() {
     let span;
     let product;
     let valueProduct;
+    let section;
+    let title;
 
-    span = document.getElementById("fiche");
+    let main = document.getElementById("main");
+
+    section = document.createElement("section");
+    section.className = "ficheGrid";
+    main.appendChild(section);
+
+    title = document.createElement("h2");
+    title.className = "hideTitle";
+    title.innerHTML = "product fiche";
+    section.appendChild(title);
+
+    span = document.createElement("span");
+    span.id = "fiche";
+    section.appendChild(span);
+
     const searchParams = new URLSearchParams(query);
     valueProduct = searchParams.get("product");
     for (let i = 0; i < producten.length; i++) {
